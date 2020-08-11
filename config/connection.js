32b4acc3fc9ed7bfe0f7d0f38e222f7193838769
davenportnw <davenportnw@gmail.com>
2020-08-11 09:09:33 -0500

    Add handlebars to document

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+// HEROKU PORT
+var PORT = process.env.PORT || 8080;
+//Create db connection 
+
+let connection =  mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user: "root",
+    password: "d0Lc3d23!",
+    database: "hamburguesa_db"
+    })
+    
+    connection.connect(function(err) {
+        if(err) throw err;
+        console.log("You are connected to your database!" );
+    })
+
+
+    module.exports = connection;
+
+    
+
--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const app = express();
 const mysql = require('mysql');
-// HEROKU PORT
-var PORT = process.env.PORT || 8080;
-//Create db connection 
+const exphbs = require('express-handlebars');
+// const routes = require('./routes');
+const connectDatabase = require('./config/connection');
+// const routes = require('./routes')(app);
 
-let connection =  mysql.createConnection({
-    host:"localhost",
-    port: 3306,
-    user: "root",
-    password: "d0Lc3d23!",
-    database: "hamburguesa_db"
-    })
-    
-    connection.connect(function(err) {
-        if(err) throw err;
-        console.log("You are connected to your database!");
-    })
+// HEROKU PORT
+// var PORT = process.env.PORT || 8080;
+const PORT = 3000;
+
+
+// app.use(express.static('public/assets'));
+app.use(express.json());
+
+
+//Connect Handlebars 
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// layoutsDir: __dirname + './views'
+//Routes
+
+app.get('/', function(req,res) {
+    res.render('burgers', {layout: false});
+})
+
+app.listen(PORT, function() {
+    console.log("Server running on port " + PORT );
+});
+
+
